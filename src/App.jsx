ba7974e92360d6312c2757860fc286870462936f
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-06-22 20:22:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import gambar from "./assets/ai-generated-8149814.jpg";
+
+function App() {
+  return (
+    <div className="carbox">
+      <img src={gambar} alt="gambar"></img>
+      <h1>Irfan Hidayatulah Putra</h1>
+      <p>This is a brief description about Irfan Hidayatulah Putra. The image above is AI-generated and represents an artistic portrayal related to the content.</p>
+      <footer className="footer">
+        <p>&copy; 2024 Irfan Hidayatulah Putra. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
